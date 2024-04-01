a07dab3e3ab7cc057e5d9a91e89d5181ee986ca5
Stanislaus <flysirin@gmail.com>
2024-04-01 13:41:06 +0400

    add README.md

---
+++ README.md
@@ -0,0 +1,25 @@
+# Cutting images into four parts
+
+Cut images into 4 equal parts using Pillow
+
+### Installation Python 3.12
+
+This project requires Python 3.12. You can download and install Python 3.12 from the [official Python website](https://www.python.org/downloads/).
+
+### How to use
+
+```
+pip install -r requirements.txt  
+```
+In main.py change source_path, output_path if you need, create source_path dir.  
+Global environment CUT_AROUND_IMG - used for cutting output parts in a circle.   
+allowed_files - a list of file formats that you want to be processed.  
+Here you can find out what other pharmaceuticals Pillow can work with:  
+https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html
+
+### Run project
+```
+python main.py  
+```
+
+
--- main.py
+++ main.py
@@ -1,42 +1,48 @@
 from pathlib import Path
 from PIL import Image
 
-allowed_files = ['.jpg', '.jpeg', '.png']
-source_path = 'source_images'
-output_path = 'output_images'
-CUT_AROUND_IMG = 2
+allowed_files: list = ['.jpg', '.jpeg', '.png', '.bmp']
+source_path: str = 'source_images'
+output_path: str = 'output_images'
+CUT_AROUND_IMG: int = 2
 
 
-def crop_image(file: Path) -> Image:
+def crop_image(file: Path, cut_around: int = 0) -> Image:
+    cut = cut_around
     current_suffix = file.suffix
-    with Image.open(file) as img:
 
+    with Image.open(file) as img:
         width, height = img.size
-        width = width - CUT_AROUND_IMG
-        height = height - CUT_AROUND_IMG
-        part_width = width // 2 - CUT_AROUND_IMG
-        part_height = height // 2 - CUT_AROUND_IMG
+        width = width
+        height = height
+        part_width = width // 2
+        part_height = height // 2
 
         coordinates = [
-            (CUT_AROUND_IMG, CUT_AROUND_IMG, part_width, part_height),  # Top left
-            (part_width, CUT_AROUND_IMG, width, part_height),  # Top right
-            (CUT_AROUND_IMG, part_height, part_width, height),  # Bottom left
-            (part_width, part_height, width, height)  # Bottom right
+            (cut, cut, part_width - cut, part_height - cut),  # Top left
+            (part_width + cut, cut, width - cut, part_height - cut),  # Top right
+            (cut, part_height + cut, part_width - cut, height - cut),  # Bottom left
+            (part_width + cut, part_height + cut, width - cut, height - cut)  # Bottom right
         ]
-        print(coordinates)
         parts = [img.crop(coord) for coord in coordinates]
         for i, part in enumerate(parts):
             part.save(f"{output_path}//{file.stem}//{file.name}_{i + 1}.{current_suffix}")
 
 
-def cut_images_from_dir(source_dir: str = source_path, output_dir: str = output_path) -> None:
+def cut_images_from_dir(source_dir: str = source_path,
+                        output_dir: str = output_path,
+                        cut_around: int = 0) -> None:
+
     folder = Path(source_dir)
     Path(f"{output_dir}//").mkdir(parents=True, exist_ok=True)
 
     for file in folder.iterdir():
         if file.is_file() and file.suffix in allowed_files:
             Path(f"{output_dir}//{file.stem}").mkdir(exist_ok=True)
-            crop_image(file)
+            crop_image(file, cut_around=cut_around)
 
 
-cut_images_from_dir()
+if __name__ == '__main__':
+    cut_images_from_dir(source_dir=source_path,
+                        output_dir=output_path,
+                        cut_around=CUT_AROUND_IMG)
